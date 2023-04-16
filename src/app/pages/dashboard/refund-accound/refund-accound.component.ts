43f0f0e56edbb6095232e58028cf534faffa7e3a
Riana Ben Andriarinaivo <rianaben002@gmail.com>
2023-04-16 10:26:34 +0300

    rectif size child component

--- src/app/pages/dashboard/refund-accound/refund-accound.component.ts
+++ src/app/pages/dashboard/refund-accound/refund-accound.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-refund-accound',
   templateUrl: './refund-accound.component.html',
-  styleUrls: ['./refund-accound.component.scss']
+  styleUrls: ['./refund-accound.component.scss'],
+  host: {'class': 'w-full'}
 })
 export class RefundAccoundComponent implements OnInit {
 
