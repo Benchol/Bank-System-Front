3a64b2486149c8344f94c74a7bab68ce4d886e16
Riana Ben Andriarinaivo <rianaben002@gmail.com>
2023-04-16 10:08:32 +0300

    navigation in dashboard ok

--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
+import { RefundAccoundComponent } from './refund-accound/refund-accound.component';
+import { TransferMoneyComponent } from './transfer-money/transfer-money.component';
+import { WithdrawMoneyComponent } from './withdraw-money/withdraw-money.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+      path: '',
+      component: DashboardComponent
+    }, {
+      path: 'refund',
+      component: RefundAccoundComponent
+    }, {
+      path: 'transfer',
+      component: TransferMoneyComponent
+    }, {
+      path: 'withdraw',
+      component: WithdrawMoneyComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -3,15 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { RefundAccoundComponent } from './refund-accound/refund-accound.component';
+import { WithdrawMoneyComponent } from './withdraw-money/withdraw-money.component';
+import { TransferMoneyComponent } from './transfer-money/transfer-money.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    RefundAccoundComponent,
+    WithdrawMoneyComponent,
+    TransferMoneyComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    RouterModule
   ]
 })
 export class DashboardModule { }
