4fd23ff9a99cb40cf0ca6767a53f3accb7401fdd
Riana Ben Andriarinaivo <rianaben002@gmail.com>
2023-04-16 11:34:46 +0300

    redund compo ok

--- src/app/pages/dashboard/refund-accound/refund-accound.component.ts
+++ src/app/pages/dashboard/refund-accound/refund-accound.component.ts
@@ -6,7 +6,7 @@ import { MessageService } from 'primeng/api';
   selector: 'app-refund-accound',
   templateUrl: './refund-accound.component.html',
   styleUrls: ['./refund-accound.component.scss'],
-  host: {'class': 'w-full'}
+  host: {'class': 'w-full relative'}
 })
 export class RefundAccoundComponent implements OnInit {
 
@@ -23,7 +23,7 @@ export class RefundAccoundComponent implements OnInit {
 
   initForm() {
     this.refundForm = this.formBuilder.group({
-      amount: [0, [Validators.required, Validators.pattern(/[0-9]{4,4}/)]],
+      amount: ['', [Validators.required, Validators.pattern(/[0-9]{4,4}/)]],
       pin: ['', [Validators.required, Validators.pattern(/[0-9]{4,4}/)]]
     })
   }
